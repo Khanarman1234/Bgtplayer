631f73d312dbcdf4d8300e4c89ca0bab91dcbad0
Khanarman1234 <135990290+Khanarman1234@users.noreply.github.com>
2024-01-31 15:56:43 +0530

    Update call.py

--- Bikash/core/call.py
+++ Bikash/core/call.py
@@ -56,7 +56,7 @@ async def _clear_(chat_id):
 class Call(PyTgCalls):
     def __init__(self):
         self.userbot1 = Client(
-            name="BGTXD1",
+            name="_king_hyt",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
